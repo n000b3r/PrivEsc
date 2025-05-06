2634aa146fecc565c91535056204cefa60935ffe
WQ <2003128@sit.singaporetech.edu.sg>
2025-05-06 06:07:35 -0400

    Disables “Remote UAC” token filtering for built‑in or local administrator accounts

--- Windows/post_scripts/post_admin.ps1
+++ Windows/post_scripts/post_admin.ps1
@@ -15,6 +15,10 @@ Write-Host "Removing Windows Defender definitions..."
 Write-Host "Enabling Remote Desktop..."
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 
+Write-Host "Enabling LocalAccountTokenFilterPolicy..."
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
+    /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
+
 Write-Host "Creating local admin account 'bill'..."
 net user bill P@ssw0rd123! /ADD
 net localgroup Administrators bill /add
